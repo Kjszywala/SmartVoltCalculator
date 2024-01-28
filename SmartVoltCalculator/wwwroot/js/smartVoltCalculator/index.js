5b1a729ba13abbea0a5a07b94218d52f92d5a546
kamil <kamil@wordshop.co.uk>
2024-01-28 13:49:40 +0000

    corrections to js file

--- SmartVoltCalculator/wwwroot/js/smartVoltCalculator/index.js
+++ SmartVoltCalculator/wwwroot/js/smartVoltCalculator/index.js
@@ -1,87 +1,15 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('calculator', () => ({
-        label: 'label calc',
-        async init() {
-            console.log('Init in calculator');
-        },
-        series: [35.1, 23.5, 2.4, 5.4],
-        colors: ["#1C64F2", "#16BDCA", "#FDBA8C", "#E74694"],
+        monthlyElectricityUsage: 168,
+        priceKWH: 28.6,
+        sum: this.monthlyElectricityUsage * (this.priceKWH / 100),
         chart: {
-            height: 320,
+            height: 400,
             width: "100%",
             type: "donut",
         },
-        stroke: {
-            colors: ["transparent"],
-            lineCap: "",
-        },
-        plotOptions: {
-            pie: {
-                donut: {
-                    labels: {
-                        show: true,
-                        name: {
-                            show: true,
-                            fontFamily: "Inter, sans-serif",
-                            offsetY: 20,
-                        },
-                        total: {
-                            showAlways: true,
-                            show: true,
-                            label: "Unique visitors",
-                            fontFamily: "Inter, sans-serif",
-                            formatter: function (w) {
-                                const sum = w.globals.seriesTotals.reduce((a, b) => {
-                                    return a + b
-                                }, 0)
-                                return `${sum}k`
-                            },
-                        },
-                        value: {
-                            show: true,
-                            fontFamily: "Inter, sans-serif",
-                            offsetY: -20,
-                            formatter: function (value) {
-                                return value + "k"
-                            },
-                        },
-                    },
-                    size: "80%",
-                },
-            },
-        },
-        grid: {
-            padding: {
-                top: -2,
-            },
-        },
-        labels: ["Direct", "Sponsor", "Affiliate", "Email marketing"],
-        dataLabels: {
-            enabled: false,
-        },
-        legend: {
-            position: "bottom",
-            fontFamily: "Inter, sans-serif",
-        },
-        yaxis: {
-            labels: {
-                formatter: function (value) {
-                    return value + "k"
-                },
-            },
-        },
-        xaxis: {
-            labels: {
-                formatter: function (value) {
-                    return value  + "k"
-                },
-            },
-            axisTicks: {
-                show: false,
-            },
-            axisBorder: {
-                show: false,
-            },
-        },
+        async init() {
+            console.log('Init in calculator');
+        }
     }))
 })
