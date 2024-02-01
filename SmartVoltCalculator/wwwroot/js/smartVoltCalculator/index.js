d27a1944fcacde4e1c6098973c9eb2edea079059
kamil <kamil@wordshop.co.uk>
2024-02-01 21:56:20 +0000

    solar panels working

--- SmartVoltCalculator/wwwroot/js/smartVoltCalculator/index.js
+++ SmartVoltCalculator/wwwroot/js/smartVoltCalculator/index.js
@@ -1,8 +1,10 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('calculator', () => ({
-        monthlyElectricityUsage: 168,
+        monthlyElectricityUsage: 280,
         priceKWH: 28.6,
         sum: 0,
+        estimatedSolarSystemSize: 0,
+        averageDailySunlightHours: 0,
         async init() {
             console.log('Init in calculator');
             this.sum = this.monthlyElectricityUsage * (this.priceKWH / 100);
